d8e2b394592ab80cda5ddda8fe93d1c0a19b4d55
zihni_wahyu <52727489+Zihni200108@users.noreply.github.com>
2022-10-28 17:24:35 +0700

    Update tambahbarang.php

--- tambahbarang.php
+++ tambahbarang.php
@@ -67,7 +67,7 @@
 
   <!-- Footer -->
   <footer class="page-footer">
-    <div class="footer">© smallFarm, 2020</div>
+    <div class="footer">© smallFarm</div>
   </footer>
   <!-- Akhir footer -->
 
